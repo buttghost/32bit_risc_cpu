1060e0b272ed43075b15c43cd7f4a6625f6e745a
yuYaoBin <buttghost0@gmail.com>
2017-11-01 17:58:31 -0700

    added PC, included jump and branch

---
+++ cpu_risc_32bit.srcs/sources_1/new/PC.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/25/2017 06:00:53 PM
+// Design Name: 
+// Module Name: PC
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module PC(
+    output reg[31:0] Count,
+    input [31:0] Jump,
+    input [31:0] Branch,
+    input sel_j,
+    input sel_b,
+    input clk,
+    input reset
+    );
+    wire [1:0] sel = {sel_j, sel_b};
+    always@(posedge clk or negedge reset)
+    begin
+        if (~reset)
+            Count <= 0;  // start the program from the beginning
+        else
+            begin
+                casex(sel)
+                2'b00:  Count <= Count + 1; // regular proceed
+                2'bx1:  Count <= Branch;    // next PC branch
+                2'b10:  Count <= Jump;      // next PC jump
+                default:    Count <= Count + 1;
+                endcase
+            end
+    end
+endmodule
